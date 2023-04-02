9a1f2dfcfe35072b31d8908963591e36e4bb1f93
Burak BOZ <burakbozb1@gmail.com>
2023-04-02 18:45:08 +0300

    Custom Pipe example

--- src/app/Models/movie.repository.ts
+++ src/app/Models/movie.repository.ts
@@ -5,10 +5,10 @@ export class MovieRepository {
 
     constructor() {
         this.movies = [
-            { id: 1, title: "Film 1", description: "film 1 açıklama", imgUrl: "1.jpg", isPopular:true, datePublish: new Date(1990,10,10) },
-            { id: 2, title: "Film 2", description: "film 2 açıklama", imgUrl: "2.jpg", isPopular:true, datePublish: new Date(1990,10,10) },
-            { id: 3, title: "Film 3", description: "film 3 açıklama", imgUrl: "3.jpg", isPopular:true, datePublish: new Date(1990,10,10) },
-            { id: 4, title: "Film 4", description: "film 4 açıklama", imgUrl: "4.jpg", isPopular:false, datePublish: new Date(1990,10,10) }
+            { id: 1, title: "Film 1", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque nesciunt aut dolorum unde, totam inventore? Cumque possimus aliquid rerum hic dolorem voluptatibus itaque iure incidunt! Voluptates autem maiores magnam praesentium.", imgUrl: "1.jpg", isPopular:true, datePublish: new Date(1990,10,10) },
+            { id: 2, title: "Film 2", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque nesciunt aut dolorum unde, totam inventore? Cumque possimus aliquid rerum hic dolorem voluptatibus itaque iure incidunt! Voluptates autem maiores magnam praesentium.", imgUrl: "2.jpg", isPopular:true, datePublish: new Date(1990,10,10) },
+            { id: 3, title: "Film 3", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque nesciunt aut dolorum unde, totam inventore? Cumque possimus aliquid rerum hic dolorem voluptatibus itaque iure incidunt! Voluptates autem maiores magnam praesentium.", imgUrl: "3.jpg", isPopular:true, datePublish: new Date(1990,10,10) },
+            { id: 4, title: "Film 4", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque nesciunt aut dolorum unde, totam inventore? Cumque possimus aliquid rerum hic dolorem voluptatibus itaque iure incidunt! Voluptates autem maiores magnam praesentium.", imgUrl: "4.jpg", isPopular:false, datePublish: new Date(1990,10,10) }
         ];
 
     }
